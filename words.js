e417ad10cfefd235a657587e7f789799543cce3a
alisonbartey <alisonbartey@gmail.com>
2025-07-31 12:23:17 -0400

    Create words.js

---
+++ words.js
@@ -0,0 +1,26 @@
+const adjectives = [
+  "Adorable", "Aggressive", "Aesthetic", "Awesome", "Brill", "Charm", "Crazy", "Debonair", "Dazzle", "Elegant",
+  "Elite", "Ethical", "Ethereal", "Fabulous", "Fantasy", "Fierce", "Fresh", "Glory", "Iconic", "Luminous",
+  "Magnificent", "Mist", "Mystic", "Peerless", "Raw", "Real", "Splendid", "Strange", "Stellar", "Stupendous",
+  "Sublime", "Swaggy", "Tender", "Terrific", "Vivacious", "Winsome", "Wondrous"
+];
+
+const nouns = [
+  "apple","ability","academy","action","actor","address","advice","animal","answer","army","author","ball",
+  "bank","beauty","bear","bed","bird","book","box","boy","brain","bread","bridge","campus","capital","car",
+  "career","castle","cause","celebration","center","challenge","chance","character","circle","city","class",
+  "climate","club","code","color","community","company","computer","concert","conversation","costume",
+  "country","culture","current","customer","cycle","danger","daughter","degree","device","dragon","dream",
+  "economy","education","energy","engine","enemy","event","example","experience","family","festival","field",
+  "figure","film","flower","food","forest","game","garden","history","hobby","holiday","home","horizon","idea",
+  "image","imagination","industry","insect","island","journal","journey","justice","king","knowledge",
+  "language","legend","library","life","light","machine","magic","market","memory","message","method","metal",
+  "mind","model","moment","mountain","music","nation","nature","network","number","object","ocean","office",
+  "operation","opinion","organism","pain","page","planet","player","poem","policy","power","practice",
+  "problem","product","program","project","promise","quality","question","reaction","reason","review","river",
+  "robot","school","science","season","signal","silver","skill","society","sound","story","strategy","student",
+  "system","technology","television","theme","tower","travel","tree","truth","universe","value","vision","voice",
+  "volume","water","weather","width","wind","woman","world","writer","year"
+];
+
+const suffixes = ["", "007", "_X", "_YT", "123", "Pro", "Dev", "TV", "99", "_God"];
